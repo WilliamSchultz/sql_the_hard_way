0c586cd54ad2864d0aa90a16ab5dee1ae5f6a215
WillaimSchultz_mac_2 <williamterrillschultz@gmail.com>
2019-12-08 18:19:26 +0300

    ex9

---
+++ ex9.sql
@@ -0,0 +1,11 @@
+UPDATE person SET first_name = 'Zed'
+  WHERE first_name = 'Hilarious Guy';
+
+UPDATE pet SET name = 'Fancy Pants '
+  WHERE id=0;
+
+UPDATE pet SET name = 'DECEASED'
+  WHERE dead=0;
+
+SELECT * FROM person;
+SELECT * FROM pet;
